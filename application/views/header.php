aae9d22295cac787233d42d697c09b87c487ab4b
sangita155 <71088584+sangita155@users.noreply.github.com>
2020-09-15 01:42:48 -0700

    Update header.php

--- application/views/header.php
+++ application/views/header.php
@@ -1,8 +1,8 @@
-
 <!DOCTYPE html>
 <html lang="en">
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <title>Redskins Training Camp Registration</title>
 <head>
 <link rel="stylesheet" href="<?php echo base_url();?>assets/css/normalize.css">
